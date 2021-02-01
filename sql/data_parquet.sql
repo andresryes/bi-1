21f599e8a2e353896b363e16fb43a037a91d9c68
andresryes <enriquebolanos@ufm.edu>
2021-01-31 21:26:44 -0600

    data parquet finished

---
+++ sql/data_parquet.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `test.dataparquet` (
+	`index` BIGINT, 
+	vendor_id TEXT, 
+	pickup_at DATETIME, 
+	dropoff_at DATETIME, 
+	passenger_count BIGINT, 
+	trip_distance FLOAT(23), 
+	rate_code_id TEXT, 
+	store_and_fwd_flag TEXT, 
+	pickup_location_id INTEGER, 
+	dropoff_location_id INTEGER, 
+	payment_type TEXT, 
+	fare_amount FLOAT(23), 
+	extra FLOAT(23), 
+	mta_tax FLOAT(23), 
+	tip_amount FLOAT(23), 
+	tolls_amount FLOAT(23), 
+	improvement_surcharge FLOAT(23), 
+	total_amount FLOAT(23), 
+	congestion_surcharge FLOAT(23)
+)
